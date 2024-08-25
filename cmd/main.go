f5d5d5bd721a43d3d05c77003d99042de3ef4251
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-08-26 00:09:58 +0200

    db config and env struct

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"log"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/tahahamdii/basic-api/cmd/api"
+	"github.com/tahahamdii/basic-api/db"
 )
 
 func main() {
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:  "root",
+		Passwd: "root",
+		Addr: "localhost:3306",
+		DBName: "basic_api",
+		Net: "tcp",
+		AllowNativePasswords: true,
+		ParseTime: true,
+	})
+
 	server := api.NewApiServer(":8080", nil)
 	if err := server.Run(); err != nil {
 		log.Fatalf("Error: %v", err)
