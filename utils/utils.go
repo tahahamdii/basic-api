69ff8d47099a379b7f68a99b8e02b03e2dc710fa
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-08-26 13:04:50 +0200

    write error  func

--- utils/utils.go
+++ utils/utils.go
@@ -20,3 +20,7 @@ func WriteJson(w http.ResponseWriter, status int , v any) error {
 
 	return json.NewEncoder(w).Encode(v)
 }
+
+func WriteError(w http.ResponseWriter, status int , err error) {
+	WriteJson(w, status,map[string]string{"error":err.Error()})
+}
