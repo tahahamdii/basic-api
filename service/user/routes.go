8b92dbf40951eb07511d34ea4f8b727e497e29ec
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-08-26 14:49:50 +0200

    registration api

--- service/user/routes.go
+++ service/user/routes.go
@@ -1,9 +1,11 @@
 package user
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/tahahamdii/basic-api/service/auth"
 	"github.com/tahahamdii/basic-api/types"
 	"github.com/tahahamdii/basic-api/utils"
 )
@@ -33,5 +35,30 @@ func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
 	}
 	
 	//check if user exists
+	_, err := h.store.GetUserByEmail(payload.Email)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with email %s already exists",payload.Email))
+		return
+	}
 
+
+	hashedPassword, err := auth.HashPassword(payload.Password)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError,err)
+		return
+	}
+	// if user doesnt exists
+
+	err = h.store.CreateUser(types.User{
+		FirstName: payload.FirstName,
+		LastName: payload.LastName,
+		Email: payload.Email,
+		Password: hashedPassword,
+	})
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError,err)
+		return
+	}
+
+	utils.WriteJson(w, http.StatusCreated, nil)
 }
