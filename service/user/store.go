a45fdb5d3deb9e272ce0bbcf47de8f047b220f62
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-09-02 21:04:12 +0200

    create user

--- service/user/store.go
+++ service/user/store.go
@@ -58,6 +58,11 @@ func (s *Store) GetUserByID(id int) (*types.User, error) {
 }
 
 func (s *Store) CreateUser(user types.User) error {
+	_, err := s.db.Exec("INSERT INTO users (firstName, lastName, email, password) VALUES (?, ?, ?, ?)", user.FirstName, user.LastName, user.Email, user.Password)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
