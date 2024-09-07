ba267d89e1e722e088a170616fca0d1e16dcf37d
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-09-07 23:29:26 +0200

    replacing the playload for handling registration

--- service/user/routes.go
+++ service/user/routes.go
@@ -26,39 +26,42 @@ func (h *Handler) RegisterRoutes(router *mux.Router) {
 func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 }
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
-
-	//get json payload
-	
-	var payload  types.RegisterUserPayload
-	if err := utils.ParseJson(r,payload); err != nil {
-		utils.WriteError(w,http.StatusBadRequest, err)
-	}
-	
-	//check if user exists
-	_, err := h.store.GetUserByEmail(payload.Email)
-	if err != nil {
-		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with email %s already exists",payload.Email))
+	var user types.RegisterUserPayload
+	if err := utils.ParseJSON(r, &user); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
 
-
-	hashedPassword, err := auth.HashPassword(payload.Password)
-	if err != nil {
-		utils.WriteError(w, http.StatusInternalServerError,err)
+	if err := utils.Validate.Struct(user); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload: %v", errors))
 		return
 	}
-	// if user doesnt exists
+
+	// check if user exists
+	_, err := h.store.GetUserByEmail(user.Email)
+	if err == nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with email %s already exists", user.Email))
+		return
+	}
+
+	// hash password
+	hashedPassword, err := auth.HashPassword(user.Password)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
 
 	err = h.store.CreateUser(types.User{
-		FirstName: payload.FirstName,
-		LastName: payload.LastName,
-		Email: payload.Email,
-		Password: hashedPassword,
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		Email:     user.Email,
+		Password:  hashedPassword,
 	})
 	if err != nil {
-		utils.WriteError(w, http.StatusInternalServerError,err)
+		utils.WriteError(w, http.StatusInternalServerError, err)
 		return
 	}
 
-	utils.WriteJson(w, http.StatusCreated, nil)
+	utils.WriteJSON(w, http.StatusCreated, nil)
 }
